fe998e04a7d2ea089e481a352f6178ab68510333
k <tohichoi@naver.com>
2018-05-11 17:56:31 +0900

    day9 tmp

--- 2015/day9/day9.py
+++ 2015/day9/day9.py
@@ -17,11 +17,13 @@ London to Belfast = 518
 Dublin to Belfast = 141
 '''
 
-# fd=StringIO.StringIO(s)
-fd=open('input.txt')
+fd=StringIO.StringIO(s)
+# fd=open('input.txt')
     
 G=nx.Graph()
 
+
+# read graph
 for line in fd.readlines():
     tokens=line.strip().split()
     if len(tokens) < 5:
@@ -29,10 +31,15 @@ for line in fd.readlines():
     
     G.add_edge(tokens[0], tokens[2], weight=int(tokens[4]))
 
-res=dict(Dijkstra(G))
+print G['London']['Belfast']['weight']
 
 nx.draw(G, with_labels = True)
 plt.show()
-# nx.draw_graphviz(G)
-
 fd.close()
+
+# for every path
+for path in nx.all_simple_paths(G):
+    dist={}
+    for n in list(G.nodes()):
+        G[n][n]['weight']
+        dist[n]=
